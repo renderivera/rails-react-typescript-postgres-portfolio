4247777aa1188d55c2da78b1bd1aa6536934bc54
raunchard <reinhard.zach@gmail.com>
2019-08-26 23:19:18 +0200

    controller name needs to be pluralized to work

--- basic-demo/config/routes.rb
+++ basic-demo/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'start#index'
 
-  resources :messages
-
+  namespace :api do
+    namespace :v1 do
+      resources :messages
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
