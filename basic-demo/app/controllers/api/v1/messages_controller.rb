1023fce326a64071e1df0be658d24b86f367fd89
raunchard <reinhard.zach@gmail.com>
2019-08-27 14:23:19 +0200

    add create API operation and message_params

--- basic-demo/app/controllers/api/v1/messages_controller.rb
+++ basic-demo/app/controllers/api/v1/messages_controller.rb
@@ -2,4 +2,13 @@ class Api::V1::MessagesController < ApplicationController
   def index
     render json: Message.all
   end
+  
+  def create (message_params)
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:user, :message)
+  end
 end
